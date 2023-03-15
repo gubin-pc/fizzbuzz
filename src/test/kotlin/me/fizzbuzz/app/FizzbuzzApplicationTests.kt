4848510091a82a8dae59727868a65d55055498b4
Pavel Gubin <gubin.pc@gmail.com>
2023-03-15 11:06:46 +0000

    add spring boot base

---
+++ src/test/kotlin/me/fizzbuzz/app/FizzbuzzApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.fizzbuzz.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FizzbuzzApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
